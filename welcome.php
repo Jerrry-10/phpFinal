709fca458e42c5c9df4ef9bc2ed2f88225af39af
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2024-05-21 08:29:35 -0400

    landing page

--- welcome.php
+++ welcome.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
-
-if (!isset($_SESSION['username'])) {
-    header("Location: login.php");
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +9,13 @@ if (!isset($_SESSION['username'])) {
     <title>Welcome</title>
 </head>
 <body>
-    <h2>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h2>
-    <a href="logout.php">Logout</a>
+    <?php if (isset($_SESSION['username'])): ?>
+        <h2>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h2>
+        <a href="logout.php">Logout</a>
+    <?php else: ?>
+        <h2>Welcome to our site!</h2>
+        <p>You are not logged in.</p>
+        <button onclick="window.location.href='login.php'">Login</button>
+    <?php endif; ?>
 </body>
 </html>
